c1fb70e9e494a2d46ee945b27b04c4c83c112a43
thekodu <praveenrao116@gmail.com>
2023-03-01 11:41:06 +0530

    added new project

---
+++ english-dictionary/index.js
@@ -0,0 +1,22 @@
+const inoutEl = document.getElementById("input");
+
+const infoTextEl = document.getElementById("info-text");
+
+async function fetchAPI(word){
+    try {
+        infoTextEl.style.display = "block";
+        infoTextEl.innerText = `Searching the meaning of "${word}"`
+        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+        const result = await fetch(url).then((res)=> res.json());
+
+        infoTextEl.style.display = "none";
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+inoutEl.addEventListener("keyup", (e)=>{
+    if(e.target.value && e.key === "Enter"){
+        fetchAPI(e.target.value)
+    }
+});
